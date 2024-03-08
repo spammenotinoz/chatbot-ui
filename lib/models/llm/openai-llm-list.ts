26a9afd0e28438ce3387a3685c51c1f882ebd4ec
spammenotinoz <spammenotinoz@gmail.com>
2024-03-08 17:15:30 +1100

    Update openai-llm-list.ts

--- lib/models/llm/openai-llm-list.ts
+++ lib/models/llm/openai-llm-list.ts
@@ -4,16 +4,6 @@ const OPENAI_PLATORM_LINK = "https://platform.openai.com/docs/overview"
 
 // OpenAI Models (UPDATED 1/25/24) -----------------------------
 
-// GPT-4 Turbo (UPDATED 1/25/24)
-const GPT4Turbo: LLM = {
-  modelId: "gpt-3.5-turbo",
-  modelName: "GPT-4 Turbo",
-  provider: "openai",
-  hostedId: "gpt-3.5-turbo",
-  platformLink: OPENAI_PLATORM_LINK,
-  imageInput: false
-}
-
 // GPT-4 Vision (UPDATED 12/18/23)
 const GPT4Vision: LLM = {
   modelId: "gpt-4-vision-preview",
@@ -38,6 +28,16 @@ const GPT4: LLM = {
 const GPT3_5Turbo: LLM = {
   modelId: "gpt-3.5-turbo",
   modelName: "GPT-3.5 Turbo",
+  provider: "openai",
+  hostedId: "gpt-3.5-turbo",
+  platformLink: OPENAI_PLATORM_LINK,
+  imageInput: false
+}
+
+// GPT-4 Turbo (UPDATED 1/25/24)
+const GPT4Turbo: LLM = {
+  modelId: "gpt-3.5-turbo",
+  modelName: "GPT-4 Turbo",
   provider: "openai",
   hostedId: "gpt-3.5-turbo",
   platformLink: OPENAI_PLATORM_LINK,
