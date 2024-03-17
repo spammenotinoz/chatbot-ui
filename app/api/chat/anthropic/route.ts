4b13ec3c1f56fa4079915b595342b7be1a1609aa
spammenotinoz <spammenotinoz@gmail.com>
2024-03-17 23:42:57 +1100

    Update route.ts

--- app/api/chat/anthropic/route.ts
+++ app/api/chat/anthropic/route.ts
@@ -4,7 +4,7 @@ import { ChatSettings } from "@/types"
 import Anthropic from "@anthropic-ai/sdk"
 import { AnthropicStream, StreamingTextResponse } from "ai"
 
-export const runtime = "experimental-edge"
+//export const runtime = "experimental-edge"
 
 export async function POST(request: Request) {
   const json = await request.json()
