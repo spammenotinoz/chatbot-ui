f436a442a191adf8394f7d3d23577879a481e30b
spammenotinoz <spammenotinoz@gmail.com>
2024-03-18 00:00:42 +1100

    Update route.ts

--- app/api/chat/anthropic/route.ts
+++ app/api/chat/anthropic/route.ts
@@ -4,12 +4,16 @@ import { ChatSettings } from "@/types";
 import Anthropic from "@anthropic-ai/sdk";
 import { AnthropicStream, StreamingTextResponse } from "ai";
 
-export default async function handler(req) {
-  if (req.method !== "POST") {
+export const config = {
+  runtime: "edge",
+};
+
+export default async function handler(request: Request) {
+  if (request.method !== "POST") {
     return new Response("Method not allowed", { status: 405 });
   }
 
-  const json = await req.json();
+  const json = await request.json();
   const { chatSettings, messages } = json as {
     chatSettings: ChatSettings;
     messages: any[];
@@ -17,14 +21,11 @@ export default async function handler(req) {
 
   try {
     const profile = await getServerProfile();
-
     checkApiKey(profile.anthropic_api_key, "Anthropic");
 
     let ANTHROPIC_FORMATTED_MESSAGES: any = messages.slice(1);
 
-    const anthropic = new Anthropic({
-      apiKey: profile.anthropic_api_key || ""
-    });
+    const anthropic = new Anthropic({ apiKey: profile.anthropic_api_key || "" });
 
     const response = await anthropic.messages.create({
       model: chatSettings.model,
@@ -32,24 +33,25 @@ export default async function handler(req) {
       temperature: chatSettings.temperature,
       system: messages[0].content,
       max_tokens: CHAT_SETTING_LIMITS[chatSettings.model].MAX_TOKEN_OUTPUT_LENGTH,
-      stream: true
+      stream: true,
     });
 
     const stream = AnthropicStream(response);
-
     return new StreamingTextResponse(stream);
-  } catch (error) {
+  } catch (error: any) {
     let errorMessage = error.message || "An unexpected error occurred";
     const errorCode = error.status || 500;
 
     if (errorMessage.toLowerCase().includes("api key not found")) {
-      errorMessage = "Anthropic API Key not found. Please set it in your profile settings.";
+      errorMessage =
+        "Anthropic API Key not found. Please set it in your profile settings.";
     } else if (errorCode === 401) {
-      errorMessage = "Anthropic API Key is incorrect. Please fix it in your profile settings.";
+      errorMessage =
+        "Anthropic API Key is incorrect. Please fix it in your profile settings.";
     }
 
     return new Response(JSON.stringify({ message: errorMessage }), {
-      status: errorCode
+      status: errorCode,
     });
   }
 }
