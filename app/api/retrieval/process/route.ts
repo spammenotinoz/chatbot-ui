7e8ba7bb9b3fbe28374a2b8d9ef479ec8f63b1d7
spammenotinoz <spammenotinoz@gmail.com>
2024-03-18 16:22:38 +1100

    Update route.ts

--- app/api/retrieval/process/route.ts
+++ app/api/retrieval/process/route.ts
@@ -104,8 +104,7 @@ export async function POST(req: Request) {
       openai = new OpenAI({
         apiKey: profile.openai_api_key || "",
         organization: profile.openai_organization_id,
-		baseURL: "https://api.openai.com/v1/embeddings"
-		//...(process.env.ENDPOINT_OPENAI && { baseURL: process.env.ENDPOINT_OPENAI })
+		...(process.env.ENDPOINT_OPENAI && { baseURL: process.env.ENDPOINT_OPENAI })
       })
     }
 
