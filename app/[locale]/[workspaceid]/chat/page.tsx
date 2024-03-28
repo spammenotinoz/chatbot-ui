86ed849b3aecce996b6c5ab1aa6c9067401ef1b3
spammenotinoz <spammenotinoz@gmail.com>
2024-03-28 13:49:00 +1100

    Update page.tsx

--- app/[locale]/[workspaceid]/chat/page.tsx
+++ app/[locale]/[workspaceid]/chat/page.tsx
@@ -32,7 +32,7 @@ export default function ChatPage() {
             <Brand theme={theme === "dark" ? "dark" : "light"} />
           </div>
 
-          <div className="absolute right-2 top-2">
+          <div className="absolute left-2 top-2">
             <ChatSettings />
           </div>
 
