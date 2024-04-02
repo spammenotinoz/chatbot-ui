dc049002e01dce22235947fdf4fcf3bc10cadbe1
spammenotinoz <spammenotinoz@gmail.com>
2024-04-02 12:39:22 +1100

    Update page.tsx

--- app/[locale]/login/page.tsx
+++ app/[locale]/login/page.tsx
@@ -146,13 +146,6 @@ export default async function Login({
           Login
         </SubmitButton>
 
-        <SubmitButton
-          formAction={signUp}
-          className="border-foreground/20 mb-2 rounded-md border px-4 py-2"
-        >
-          Sign Up
-        </SubmitButton>
-
         <div className="text-muted-foreground mt-1 flex justify-center text-sm">
           <span className="mr-1">Forgot your password?</span>
           <button
